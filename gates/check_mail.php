62e0eb1857382d5ae245020d2b1fd7582c3165b3
IgorKampusano <56488719+IgorKampusano@users.noreply.github.com>
2021-07-18 12:35:57 +0300

    Delete gates directory

--- gates/check_mail.php
+++
@@ -1,5 +0,0 @@
-<?php
-	print mail("name@my.ru", "header", "text");
-
-    if(mail("kampusano.igor@mail.ru", "My Subject", "Line 1\nLine 2\nLine 3", "-f kampusano.igor@mail.ru")) echo "message send";
-    else echo "message not send";
