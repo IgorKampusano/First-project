c46349a801446e0569c82b6de132fcd83f9b0d60
IgorKampusano <56488719+IgorKampusano@users.noreply.github.com>
2021-08-03 10:44:41 +0300

    Update send_mail.php

--- greatgate/send_mail.php
+++ greatgate/send_mail.php
@@ -22,13 +22,13 @@ $_POST["работы-3"];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'igor.kampusano@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'microsoft3'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = ''; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = ''; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('igor.kampusano@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('kampusano.igor@mail.ru');     // Кому будет уходить письмо
+$mail->setFrom(''); // от кого будет уходить письмо?
+$mail->addAddress('');     // Кому будет уходить письмо
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
